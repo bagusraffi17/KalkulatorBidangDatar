6462a1848249719da9c66011446c16b367e525e9
Imanuel Khrisna Ananda <111202013030@mhs.dinus.ac.id>
2022-10-21 23:22:56 +0700

    Initial commit

---
+++ KalkulatorBidangDatar/app/src/main/java/com/example/kalkulatorbidangdatar/Persegi.java
@@ -0,0 +1,14 @@
+package com.example.kalkulatorbidangdatar;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Persegi extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_persegi);
+    }
+}
